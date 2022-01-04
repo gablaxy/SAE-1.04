caa0c9e5a1a72d0478b569bbd0a5b2e240daef32
gablaxy <32848524+gablaxy@users.noreply.github.com>
2022-01-04 16:09:40 +0100

    type article

--- matski_update.sql
+++ matski_update.sql
@@ -1,2 +1,17 @@
 create schema matski_update;
 set schema 'matski_update';
+
+/*==============================================================*/
+/* Table : TYPEARTICLE                                          */
+/*==============================================================*/
+create table TYPEARTICLE (
+   CODETYPE             CHAR(1)              not null,
+   LIBELLETYPE          VARCHAR(40)          null,
+   constraint PK_TYPEARTICLE primary key (CODETYPE)
+);
+Insert into TYPEARTICLE (CODETYPE,LIBELLETYPE) values
+('C','Consommable');
+Insert into TYPEARTICLE (CODETYPE,LIBELLETYPE) values
+('P','Produit Stockable');
+Insert into TYPEARTICLE (CODETYPE,LIBELLETYPE) values
+('S','Service');
