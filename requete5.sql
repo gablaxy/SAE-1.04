73e5dd607a2f5695da10e93ac2a2dcc722af0634
Naiizey <79869581+Naiizey@users.noreply.github.com>
2022-01-19 11:14:33 +0100

    Update requete5.sql

--- requete5.sql
+++ requete5.sql
@@ -1,12 +1,12 @@
 set schema 'matski_update';
 select sum(montantttc - montantht)
 from commande natural join etiquette
-where codetypetva = '1'; 
+where codetypetva = '1' and datecommande >= current_date - 30;
 
 select sum(montantttc - montantht)
 from commande natural join etiquette
-where codetypetva = '2'; 
+where codetypetva = '2' and datecommande >= current_date - 30; 
 
 select sum(montantttc - montantht)
 from commande natural join etiquette
-where codetypetva = '3'; 
+where codetypetva = '3' and datecommande >= current_date - 30;
