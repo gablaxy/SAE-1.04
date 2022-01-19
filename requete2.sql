c87f4e830c58fd88882543be301e5df917b35b43
Naiizey <79869581+Naiizey@users.noreply.github.com>
2022-01-19 11:14:10 +0100

    Update requete2.sql

--- requete2.sql
+++ requete2.sql
@@ -1,8 +1,12 @@
 set schema 'matski_update';
-select codeetiquette, datecommande
+select codeetiquette, datecommande, numcommande
 from client natural join commande
 where codeetiquette = 'CH' and datecommande = '2021-12-22';
 
-select codeetiquette, datecommande
+select codeetiquette, datecommande, numcommande
 from client natural join commande
 where codeetiquette = 'CH'
+except
+select codeetiquette, datecommande, numcommande
+from client natural join commande
+where codeetiquette = 'CH' and datecommande = '2021-12-22';
